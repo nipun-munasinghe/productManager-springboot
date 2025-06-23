20657a4fa1bef74e46bd770b14ab31e88ac879a3
nipun-munasinghe <nipunlakmal884@gmail.com>
2025-06-23 20:51:41 +0530

    Add ProductDTO and update Product model annotations
    Introduced ProductDTO class for data transfer purposes. Updated Product model to use explicit @Getter and @Setter annotations instead of @Data.

---
+++ src/main/java/com/products/productmanager/dto/ProductDTO.java
@@ -0,0 +1,15 @@
+package com.products.productmanager.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDTO {
+    private Integer id;
+    private String name;
+    private double price;
+    private String description;
+}
