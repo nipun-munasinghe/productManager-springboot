c0763a44de3dd96cbcaea856d3c69b9b4d93eab2
nipun-munasinghe <nipunlakmal884@gmail.com>
2025-06-24 13:15:33 +0530

    Add ProductController with create product endpoint
    Introduces ProductController to handle product-related API requests. Adds a POST endpoint to create new products using ProductService.

---
+++ src/main/java/com/products/productmanager/controller/ProductController.java
@@ -0,0 +1,20 @@
+package com.products.productmanager.controller;
+
+import com.products.productmanager.dto.ProductDTO;
+import com.products.productmanager.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductDTO createProduct(@RequestBody ProductDTO productDTO) {
+        return productService.createProduct(productDTO);
+    }
+}
